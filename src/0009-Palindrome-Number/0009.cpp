bdf9535d9d63a7b9b743d70a6dbbe9e90de836c9
JuJiaHe <58724756+Kanna-jiahe@users.noreply.github.com>
2020-03-04 19:06:23 +0800

    add 9

---
+++ src/0009-Palindrome-Number/0009.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		if (x < 0)return false;
+		if (x >= 0 && x < 10)return true;
+		long long temp = 0;
+		int xx = x;
+		while (x != 0)
+		{
+			int n = x % 10;
+			temp = temp * 10 + n;
+			x /= 10;
+		}
+		if (temp == xx)return true;
+		else return false;
+	}
+};
